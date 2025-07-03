6827fb62017f70469aae1e1b5f753f92d8bba54a
Sriram.T <sriramagaram07@gmail.com>
2025-07-03 16:06:49 +0530

    for white screen

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,16 +1,19 @@
-import { Component } from '@angular/core';
-import {  RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
 import { ReactiveFormsModule } from '@angular/forms';
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , NavComponent, ReactiveFormsModule ],
+  imports: [RouterOutlet, NavComponent, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
+
+  ngOnInit() {
+    console.log('AppComponent loaded!');
+  }
 }
